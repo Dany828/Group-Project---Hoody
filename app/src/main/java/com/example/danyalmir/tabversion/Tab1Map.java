d2835300f1c615ba65cb02c473aee26ed51a3796
Dany828 <dmir828@gmail.com>
2018-04-01 22:56:11 +0100

    Initial commit

---
+++ app/src/main/java/com/example/danyalmir/tabversion/Tab1Map.java
@@ -0,0 +1,30 @@
+package com.example.danyalmir.tabversion;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class Tab1Map extends Fragment {
+
+    WebView webView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.tab1map, container, false);
+
+        webView = (WebView) rootView.findViewById(R.id.web);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+        return rootView;
+
+    }
+
+}
---
+++ app/src/main/java/com/example/danyalmir/tabversion/Tab3About.java
@@ -0,0 +1,18 @@
+package com.example.danyalmir.tabversion;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab3About extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab3about, container, false);
+        return rootView;
+    }
+
+}
